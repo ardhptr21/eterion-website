425cf57f43a2e97e9297f32c0c6f2848ed59ffa7
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-06-29 18:46:37 +0700

    feat: create faces section at homepage

--- src/app/(container)/Faces.tsx
+++ src/app/(container)/Faces.tsx
@@ -1,3 +1,46 @@
+"use client";
+
+import MemberCard from "@/components/cards/MemberCard";
+import members from "@/statics/members.json";
+import AutoScroll from "embla-carousel-auto-scroll";
+import useEmblaCarousel from "embla-carousel-react";
+
 export default function Faces() {
-  return <section>{/* TODO: Faces Eterion section */}</section>;
+  const [emblaRef] = useEmblaCarousel(
+    {
+      loop: true,
+      align: "start",
+      dragFree: true,
+      duration: 0,
+      skipSnaps: false,
+    },
+    [
+      AutoScroll({
+        stopOnInteraction: false,
+        stopOnFocusIn: true,
+        startDelay: 0,
+        speed: 7,
+      }),
+    ]
+  );
+
+  return (
+    <section className="py-20">
+      <div className="flex justify-end">
+        <h1 className="text-5xl font-bold font-nexa">Faces of Eterion</h1>
+      </div>
+      <div className="overflow-hidden mt-10 cursor-grab" ref={emblaRef}>
+        <div className="flex gap-5 px-5">
+          {members.map((member) => (
+            <MemberCard
+              key={member.nama}
+              name={member.nama}
+              ig={member.ig || ""}
+              image={String(member.nrp).slice(7, 10) + ".jpg"}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
