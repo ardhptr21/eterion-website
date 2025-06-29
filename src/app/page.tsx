5693067bbc27e6cc9a4b2a0219d4e149b5b46e57
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-06-29 18:46:37 +0700

    feat: create faces section at homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Container from "@/components/commons/Container";
 import Hero from "./(container)/Hero";
 import Story from "./(container)/Story";
 import About from "./(container)/About";
+import Faces from "./(container)/Faces";
 
 export default function Home() {
   return (
     <Container as="main">
       <Hero />
       <About />
+      <Faces />
       <Story />
     </Container>
   );
---
+++ src/components/cards/MemberCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Noise from "../effects/Noise";
+
+interface Props {
+  name: string;
+  ig: string;
+  image: string;
+}
+
+export default function MemberCard({ name, ig, image }: Props) {
+  return (
+    <div className="min-w-sm max-w-sm shrink-0 p-10 rounded-xl border-2 border-accent relative bg-[#140c2c]/80 backdrop-blur-lg">
+      <Noise />
+      <div className="aspect-[4/5] bg-white rounded-xl z-10 relative overflow-hidden">
+        <Image
+          src={`/images/members/${image}`}
+          alt={name}
+          fill
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="mt-5 z-10">
+        <h4 className="text-3xl font-nexa font-bold">{name}</h4>
+        <h6 className="font-nexa">{ig}</h6>
+      </div>
+      <div className="absolute -z-10 inset-0 bg-gradient-to-b from-transparent from-40% via-amber-300/20 via-60% to-accent/50 rounded-xl pointer-events-none"></div>
+    </div>
+  );
+}
