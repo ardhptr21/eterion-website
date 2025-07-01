c48a6e078f9ce824f5444697d2cd6016cf40e80c
thisiskcp <syauqireza28@gmail.com>
2025-07-01 22:56:26 +0700

    feat: add statistic box

--- src/app/about/(container)/Statistic.tsx
+++ src/app/about/(container)/Statistic.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
 export default function Statistic() {
-  return <section>{/* TODO: Statistic Our Beginning and Our Members section */}</section>;
+  return (
+    <section>
+      <div className="flex gap-6 justify-center py-20">
+        <div>
+          <p className="text-center font-extralight font-nexa">Our Beginning</p>
+          <div className="relative m-1 rounded-4xl border-2 border-accent flex items-center justify-center text-center font-nexa font-bold">
+            <div className="bg-radial from-white to-transparent size-20 blur-xl absolute -z-10"></div>
+            <div className="bg-radial -top-4 right-4 from-primary to-transparent size-40 blur-xl absolute -z-10"></div>
+            <div className="bg-radial  -right-6 from-yellow-500 to-transparent size-10 blur-2xl absolute -z-10"></div>
+            <p className="text-2xl p-4 m-4">2024</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-center font-extralight font-nexa">Our Members</p>
+          <div className="relative m-1 rounded-4xl border-2 border-accent flex items-center justify-center text-center font-nexa font-bold">
+            <div className="bg-radial from-white to-transparent size-20 blur-xl absolute -z-10"></div>
+            <div className="bg-radial -top-4 right-4 from-primary to-transparent size-40 blur-xl absolute -z-10"></div>
+            <div className="bg-radial  -right-6 from-yellow-500 to-transparent size-10 blur-2xl absolute -z-10"></div>
+            <p className="text-2xl p-4 m-4">116 People</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
