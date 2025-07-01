33f78cdbb9a95470e705c71a002a7ba0a0b10718
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-07-01 19:59:59 +0700

    feat: templating page members

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,12 +1,33 @@
-import ComingSoon from "@/components/commons/ComingSoon";
 import Container from "@/components/commons/Container";
-import React from "react";
+import Buttton from "@/components/ui/Button";
+import Template from "./(container)/lists/Template";
 
 export default function Members() {
   return (
-    <Container as="main">
-      {/* NOTE: Hapus component ComingSoon kalau udah ngerjain salah satu section page */}
-      <ComingSoon />
-    </Container>
+    <main>
+      <Container as="section" className="my-32 space-y-12">
+        <h1 className="text-6xl font-nexa font-bold">Faces of Eterion</h1>
+        <p className="text-3xl font-nexa">Meet the people behind Eterion!</p>
+      </Container>
+      <Container as="section" className="my-32 relative flex items-start gap-10">
+        <div className="w-1/6 space-y-5 sticky top-10">
+          <Buttton className="w-full">001 - 030</Buttton>
+          <Buttton className="w-full">031 - 060</Buttton>
+          <Buttton className="w-full">061 - 090</Buttton>
+          <Buttton className="w-full">091 - 120</Buttton>
+        </div>
+        <div className="w-5/6 grid grid-cols-3 gap-5">
+          <Template />
+          <Template />
+          <Template />
+          <Template />
+          <Template />
+          <Template />
+          <Template />
+          <Template />
+          <Template />
+        </div>
+      </Container>
+    </main>
   );
 }
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { PropsWithChildren } from "react";
+
+interface ButtonProps extends PropsWithChildren {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Button({ children, className }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "w-max h-6 p-5 bg-gradient-to-t from-[#56C5FE] to-neutral-200 rounded-[15px] shadow-[1px_1px_10px_0px_rgba(255,255,255,1.00)] flex justify-center items-center gap-2.5 overflow-hidden hover:opacity-90 transition-opacity cursor-pointer text-gray-950 font-bold font-nexa",
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
