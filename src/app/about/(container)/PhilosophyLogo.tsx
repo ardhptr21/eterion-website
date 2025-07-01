78a779f67dd2d7d7b22f32f6b22dbd875b038d1d
zelebwr <jonathanzelig6013@gmail.com>
2025-07-01 22:18:47 +0700

    style: changed typing format

--- src/app/about/(container)/PhilosophyLogo.tsx
+++ src/app/about/(container)/PhilosophyLogo.tsx
@@ -1,8 +1,13 @@
 import LogoDisplay from "@/components/about/logo-display/LogoDisplay";
+import SectionHeader from "@/components/commons/section-header-right/SectionHeaderRight";
 
 export default function PhilosophyLogo() {
     return (
         <section>
+            <SectionHeader
+                title="Our Philosophy"
+                subtitle="The Way Behind Eterion"
+            />
             <LogoDisplay />
         </section>
     );
