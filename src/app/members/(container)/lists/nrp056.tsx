a7a8301306bb9c645d9be1daf0c6dba42ac606f6
TheoAaron <theodorusaaron31@gmail.com>
2025-07-08 18:42:04 +0800

    Changed Data 056

--- src/app/members/(container)/lists/nrp056.tsx
+++ src/app/members/(container)/lists/nrp056.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 import { useState } from "react";
 
 const data = {
-  name: "John Doe",
-  nrp: "5027241000",
-  image: "000.jpg",
-  funfact: "kalo makan pake nasi, nasinya harus dari beras",
-  hobby: "makan nasi dari beras",
-  origin: "Surabaya",
+  name: "Theodorus Aaron Ugraha",
+  nrp: "5027241056",
+  image: "056.jpg",
+  funfact: "Males Bangun Pagi",
+  hobby: "Bangun Sore",
+  origin: "Makassar",
 };
 
 export default function NRP056() {
