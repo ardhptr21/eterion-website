173b89ac633f76934fbd376047831ec359387935
Anfasa bilqi <92145990+Fasa02@users.noreply.github.com>
2025-07-13 01:02:30 +0700

    fnt2

--- src/app/members/(container)/lists/nrp054.tsx
+++ src/app/members/(container)/lists/nrp054.tsx
@@ -10,7 +10,7 @@ import localFont from 'next/font/local';
 const collegeBlock = localFont({
   src: [
     {
-      path: 'components/fonts/college/CollegeBlock.woff2',
+      path: 'fonts/CollegeBlock.woff2',
       weight: '400',
       style: 'normal',
     },
