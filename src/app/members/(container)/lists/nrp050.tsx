ac334081bdf6c713d2edf5a29b2f834eaf7b9a64
rayahmd <rayasyarif@gmail.com>
2025-07-12 08:26:53 +0700

    member card nrp 50

--- src/app/members/(container)/lists/nrp050.tsx
+++ src/app/members/(container)/lists/nrp050.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 import { useState } from "react";
 
 const data = {
-  name: "John Doe",
-  nrp: "5027241000",
-  image: "000.jpg",
-  funfact: "kalo makan pake nasi, nasinya harus dari beras",
-  hobby: "makan nasi dari beras",
-  origin: "Surabaya",
+  name: "Daniswara Fausta Novanto",
+  nrp: "5027241050",
+  image: "050.jpg",
+  funfact: "Selalu bangun sebelum alarm nyala",
+  hobby: "main game, baca novel",
+  origin: "Gresik",
 };
 
 export default function NRP050() {
