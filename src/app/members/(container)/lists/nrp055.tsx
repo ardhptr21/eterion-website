da14cb46b34c4c7951e2ef591792c24d326ccbaa
bybayu <138281388+bybayu@users.noreply.github.com>
2025-07-13 11:36:56 +0700

    Bayu 055 udah (#107)
    * Bayu 055 udah ya
    
    * 055 bayu edited

--- src/app/members/(container)/lists/nrp055.tsx
+++ src/app/members/(container)/lists/nrp055.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 import { useState } from "react";
 
 const data = {
-  name: "John Doe",
-  nrp: "5027241000",
-  image: "000.jpg",
-  funfact: "kalo makan pake nasi, nasinya harus dari beras",
-  hobby: "makan nasi dari beras",
-  origin: "Surabaya",
+  name: "Bayu Kurniawan",
+  nrp: "5027241055",
+  image: "055.jpg",
+  funfact: "Sering dikira jawa padahal asli banjar",
+  hobby: "Volly, Denger Musik",
+  origin: "Banjarmasin",
 };
 
 export default function NRP055() {
